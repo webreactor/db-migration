f55d7181cb3c7dd0b71a98bb2932f54f717214a9
Ivan Podorozhny <ivan@podorozhny.ru>
2016-02-10 18:00:14 +0300

    Utilities code style

--- src/Utilities.php
+++ src/Utilities.php
@@ -4,10 +4,8 @@ namespace Dbml;
 
 use Symfony\Component\Yaml\Yaml;
 
-class Utilities
-{
-    public static function exec($cmd)
-    {
+class Utilities {
+    public static function exec($cmd) {
         $rez = 0;
         echo "$cmd\n";
         passthru($cmd, $rez);
@@ -16,8 +14,7 @@ class Utilities
         }
     }
 
-    public static function buildCmdArgs($options, $available)
-    {
+    public static function buildCmdArgs($options, $available) {
         $rez = array();
         foreach ($options as $key => $value) {
             if (isset($available[$key]) && $value !== null) {
@@ -28,15 +25,13 @@ class Utilities
         return implode(' ', $rez);
     }
 
-    public static function loadConfig($file)
-    {
+    public static function loadConfig($file) {
         $file_options = self::parseYAML(file_get_contents($file), true);
 
         return Utilities::resolveEnv($file_options);
     }
 
-    public static function resolveEnv($data)
-    {
+    public static function resolveEnv($data) {
         if (is_array($data)) {
             foreach ($data as $key => $value) {
                 $data[$key] = self::resolveEnv($value);
@@ -55,18 +50,15 @@ class Utilities
         return $data;
     }
 
-    public static function strToClassName($str)
-    {
+    public static function strToClassName($str) {
         return str_replace(' ', '', ucwords(str_replace('-', ' ', $str)));
     }
 
-    public static function parseYAML($input, $exceptionOnInvalidType = false, $objectSupport = false, $objectForMap = false)
-    {
+    public static function parseYAML($input, $exceptionOnInvalidType = false, $objectSupport = false, $objectForMap = false) {
         return Yaml::parse($input, $exceptionOnInvalidType, $objectSupport, $objectForMap);
     }
 
-    public static function dumpYAML($array, $inline = 2, $indent = 4, $exceptionOnInvalidType = false, $objectSupport = false)
-    {
+    public static function dumpYAML($array, $inline = 2, $indent = 4, $exceptionOnInvalidType = false, $objectSupport = false) {
         return Yaml::dump($array, $inline, $indent, $exceptionOnInvalidType, $objectSupport);
     }
 }
